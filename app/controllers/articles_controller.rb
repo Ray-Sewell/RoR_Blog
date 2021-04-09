3ccc9c5d0c8e8425085cc4cebdd777d373cebaab
Ray <sewellray11+git@googlemail.com>
2021-04-09 16:26:16 +0100

    removed json builders, refactored article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
   def index
@@ -8,6 +7,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = set_article
   end
 
   # GET /articles/new
@@ -17,49 +17,41 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+    @article = set_article
   end
 
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.save
+      redirect_to @article
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    @article = set_article
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
+    @article = set_article
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to root_path
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      return Article.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
