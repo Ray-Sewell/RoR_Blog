0b7976e3a1d1ff6148388507c5757075c50fbeaa
Ray <sewellray11+git@googlemail.com>
2021-04-19 12:01:11 +0100

    added comment length validation, adjusted views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   include Visible
   belongs_to :article
 
-  validates :author, presence: true
-  validates :body, presence: true, length: { minimum: 1 }
+  validates :author, presence: true, length: { minimum: 1, maximum: 15 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 60 }
 end
